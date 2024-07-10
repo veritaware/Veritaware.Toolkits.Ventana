b026f965a5831f40004af0027925b7e8d4123c5e
Daniel Praźmo <d.prazmo@icloud.com>
2024-07-10 20:22:01 +0200

    Create the base Visual class and Windows class stubble Add CMakeLists.txt

---
+++ include/Base/Visual.hpp
@@ -0,0 +1,17 @@
+#ifndef VWRTK_VENTANA_VISUAL_HPP
+#define VWRTK_VENTANA_VISUAL_HPP
+
+#include "Object.hpp"
+
+namespace ventana
+{
+    /// <summary>
+    /// Provides a window context for rendering.
+    /// Wraps over the vwr::System::Window class.
+    /// </summary>
+    class Visual : public vwr::Object
+    {
+    };
+}
+
+#endif //VWRTK_VENTANA_VISUAL_HPP
---
+++ src/Controls/Window.cpp
@@ -0,0 +1,1 @@
+#include "Controls/Window.hpp"
